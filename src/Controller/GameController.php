e4d726e45a58299418cb89d5a2e3367a356eb2cc
Rolls47 <quentin.adadain@gmail.com>
2020-04-30 04:44:28 +0200

    excellent3

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -2,21 +2,21 @@
 
 
 namespace App\Controller;
-
 class GameController extends AbstractController
 {
-    public function index()
+        public function index()
     {
-        if (($_POST['pseudo'])) {
+        if (!empty($_POST['pseudo'])) {
             $_SESSION['pseudo'] = $_POST['pseudo'];
-            $sessions = $_SESSION;
-
-            return $this->twig->render('Game/indexgame.html.twig', [
-                'sessions' => $sessions,
-            ]);
+        } else {
+            $_SESSION['pseudo'] = 'Merlin';
         }
-    }
+        $sessions = $_SESSION;
 
+        return $this->twig->render('Game/indexgame.html.twig', [
+            'sessions' => $sessions,
+        ]);
+    }
     public function gameOne_1()
     {
         return $this->twig->render('Game/gameone_1.html.twig');
