86c9bbb945e7ae5e64d0a60c57da0378b8261109
TimoArdi <t.ardilouze@gmail.com>
2020-04-30 02:05:19 +0200

    Add HomePageFeature with Login

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -7,6 +7,13 @@ class GameController extends AbstractController
 {
     public function index()
     {
-        return $this->twig->render('Game/indexgame.html.twig');
+        if (($_POST['pseudo'])) {
+            $_SESSION['pseudo'] = $_POST['pseudo'];
+            $sessions = $_SESSION;
+
+            return $this->twig->render('Game/indexgame.html.twig', [
+                'sessions' => $sessions,
+            ]);
+        }
     }
 }
