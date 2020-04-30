ef4ecd68ba2356f53257a6b68b97de836e96b845
Green <regnault.marien@gmail.com>
2020-04-30 02:05:16 +0200

    Création de la partie jeu pour les trois challenges

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -9,4 +9,94 @@ class GameController extends AbstractController
     {
         return $this->twig->render('Game/indexgame.html.twig');
     }
+
+    public function gameOne_1()
+    {
+        return $this->twig->render('Game/gameone_1.html.twig');
+    }
+
+    public function gameOne_2()
+    {
+        return $this->twig->render('Game/gameone_2.html.twig');
+    }
+
+    public function gameOne_3()
+    {
+        return $this->twig->render('Game/gameone_3.html.twig');
+    }
+
+    public function gameOne_4()
+    {
+        return $this->twig->render('Game/gameone_4.html.twig');
+    }
+
+    public function gameOne_5()
+    {
+        return $this->twig->render('Game/gameone_5.html.twig');
+    }
+
+    public function gameOne_victoire()
+    {
+        return $this->twig->render('Game/gameone_victoire.html.twig');
+    }
+
+    public function gameTwo_1()
+    {
+        return $this->twig->render('Game/gametwo_1.html.twig');
+    }
+
+    public function gameTwo_2()
+    {
+        return $this->twig->render('Game/gametwo_2.html.twig');
+    }
+
+    public function gameTwo_3()
+    {
+        return $this->twig->render('Game/gametwo_3.html.twig');
+    }
+
+    public function gameTwo_4()
+    {
+        return $this->twig->render('Game/gametwo_4.html.twig');
+    }
+
+    public function gameTwo_5()
+    {
+        return $this->twig->render('Game/gametwo_5.html.twig');
+    }
+
+    public function gameTwo_victoire()
+    {
+        return $this->twig->render('Game/gametwo_victoire.html.twig');
+    }
+
+    public function gameThree_1()
+    {
+        return $this->twig->render('Game/gamethree_1.html.twig');
+    }
+
+    public function gameThree_2()
+    {
+        return $this->twig->render('Game/gamethree_2.html.twig');
+    }
+
+    public function gameThree_3()
+    {
+        return $this->twig->render('Game/gamethree_3.html.twig');
+    }
+
+    public function gameThree_4()
+    {
+        return $this->twig->render('Game/gamethree_4.html.twig');
+    }
+
+    public function gameThree_5()
+    {
+        return $this->twig->render('Game/gamethree_5.html.twig');
+    }
+
+    public function gameThree_victoire()
+    {
+        return $this->twig->render('Game/gamethree_victoire.html.twig');
+    }
 }
